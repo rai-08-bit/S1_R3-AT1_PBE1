07ed547a300c38dbc5de270254bf6687ccf16b3a
unknown <raissa.bez08@outlook.com>
2025-08-04 12:45:15 -0300

    Segundo commit

---
+++ Servidor de Produtos/app.js
@@ -0,0 +1,9 @@
+const express = require(`express`); // Para importar o Express e utilizar suas funcionalidades
+
+const app = express(); // Esta constante é responsável por configurar o servidor Back-end
+const PORT = 80; // Contante responsável por configurar a porta que o servidor estará sendo utilizado
+
+// Finalização do servidor, esta deve ser sempre a ÚLTIMA LINHA DO CÓDIGO!
+app.listen(PORT, ()=>{
+    console.log(`O seu servidor de produtos está rodando na porta ${PORT}`);
+});
